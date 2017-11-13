60a167327af23e887ecb49eebabd47452d3ff4e2
Pietari Kettunen <pietari@sqooba.io>
2017-11-13 09:47:07 +0100

    use config for test app as well

--- src/test/scala/io/sqooba/ConfApp.scala
+++ src/test/scala/io/sqooba/ConfApp.scala
@@ -1,13 +1,17 @@
 package io.sqooba
 
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.Logger
 import io.sqooba.conf.SqConf
 
 object ConfApp extends App {
+
+  val logger = Logger(this.getClass)
+
   val conf = new SqConf()
   val text = conf.getString("conf.text")
-  println(s"conf.text: $text")
+  logger.info(s"conf.text: $text")
   val value = ConfigFactory.load().getString("my.secret.value")
-  println(s"my.secret.value: $value")
-  println(args.toList)
+  logger.info(s"my.secret.value: $value")
+  logger.info(s"args: ${args.toList}")
 }
