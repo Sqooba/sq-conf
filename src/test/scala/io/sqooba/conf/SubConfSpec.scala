d0b6ab1a2e0ff24ff8b536aa4ffb410971cc05e4
Pietari Kettunen <pietari@sqooba.io>
2019-04-02 11:06:17 +0200

    more tests

--- src/test/scala/io/sqooba/conf/SubConfSpec.scala
+++ src/test/scala/io/sqooba/conf/SubConfSpec.scala
@@ -5,18 +5,34 @@ import org.scalatest.{FlatSpec, Matchers}
 class SubConfSpec extends FlatSpec with Matchers {
 
 	val conf = new SqConf
+	val childConf = conf.getConfig("subConf.another")
+
+	val javaConf = conf.asJava()
+	val javaChildConf = javaConf.getConfig("subConf.another")
+
 
 	"get sub config" should "give a valid sqConf" in {
-		val childConf = conf.getConfig("subConf.another")
 		childConf shouldBe a [SqConf]
+    val rootString = conf.getString("subConf.rootString")
 		val stringVal = childConf.getString("aString")
+
+    rootString shouldBe "root"
 		stringVal shouldBe "this is a string"
 	}
 
-	"sub config" should "give values with short key" in {
-		val childConf = conf.getConfig("subConf.another")
-		val stringVal = childConf.getString("aString")
-		stringVal shouldBe "this is a string"
+	it should "give int with short key" in {
+		val intVal = childConf.getInt("aInt")
+		intVal shouldBe 123
 	}
+
+  "java sub config" should "give string with short key" in {
+    val stringVal: java.lang.String = javaChildConf.getString("aString")
+    stringVal shouldBe "this is a string"
+  }
+
+  it should "give int with short key" in {
+    val intVal: java.lang.Integer = javaChildConf.getInt("aInt")
+    intVal shouldBe 123
+  }
 
 }
